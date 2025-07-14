055ac0c0f2ac7854e5d0607fdc868448fb793987
leonel Marquez <leomarqz2020@gmail.com>
2025-07-13 21:08:58 -0600

    Feature: Se agrego BookService

---
+++ src/main/java/com/leomarqz/bookstore/services/BookService.java
@@ -0,0 +1,38 @@
+package com.leomarqz.bookstore.services;
+
+import com.leomarqz.bookstore.models.Book;
+import com.leomarqz.bookstore.repositories.IBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class BookService implements IBookService{
+
+    @Autowired
+    private IBookRepository bookRepository;
+
+    @Override
+    public List<Book> listBooks() {
+        return this.bookRepository.findAll();
+    }
+
+    @Override
+    public Book findBookById(Integer id) {
+        Book book = this.bookRepository.findById(id).orElse(null);
+        return book;
+    }
+
+    @Override
+    public void createBook(Book book) {
+        this.bookRepository.save(book);
+    }
+
+    @Override
+    public void deleteBook(Integer id) {
+        if (this.bookRepository.existsById(id)) {
+            this.bookRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Book not found with id: " + id);
+        }
+    }
+}
--- src/main/java/com/leomarqz/bookstore/services/IBookService.java
+++ src/main/java/com/leomarqz/bookstore/services/IBookService.java
@@ -7,8 +7,7 @@ import java.util.List;
 public interface IBookService
 {
     List<Book> listBooks();
-    void findBookById(Integer id);
-    void createBook(String title, String author, Double price, Integer stock);
-    void updateBook(Integer id, String title, String author, Double price, Integer stock);
+    Book findBookById(Integer id);
+    void createBook(Book book);
     void deleteBook(Integer id);
 }
