266bfc715c988f84cd9418e70dfb4fb4753c3031
leonel Marquez <leomarqz2020@gmail.com>
2025-07-14 11:24:04 -0600

    Feature: Se agrego la carga de datos en la tabla de libros

--- src/main/java/com/leomarqz/bookstore/views/BookForm.java
+++ src/main/java/com/leomarqz/bookstore/views/BookForm.java
@@ -5,13 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 @Component
 public class BookForm extends JFrame
 {
     private BookService bookService;
     private JPanel panel;
+    private JTable table;
+    private DefaultTableModel tableModel;
 
     @Autowired
     public BookForm(BookService bookService){
@@ -31,5 +36,39 @@ public class BookForm extends JFrame
         int x = (screenSize.width - getWidth()) / 2;
         int y = (screenSize.height - getHeight()) / 2;
         setLocation(x, y);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        this.tableModel = new DefaultTableModel(0, 5);
+
+        String[] headers = {"Id", "Title", "Author", "Price", "Stock"};
+
+        this.tableModel.setColumnIdentifiers(headers);
+        this.table = new JTable(tableModel);
+
+        // Ajustar ancho de columna "Id"
+        table.getColumnModel().getColumn(0).setPreferredWidth(40);
+        table.getColumnModel().getColumn(0).setMaxWidth(50);
+        table.getColumnModel().getColumn(0).setMinWidth(30);
+
+        loadBooks();
+    }
+
+    private void loadBooks(){
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(Locale.US);
+
+        tableModel.setRowCount(0); // Clear existing rows
+        bookService.listBooks().forEach((book) -> {
+            Object[] row = {
+                book.getId(),
+                book.getTitle(),
+                book.getAuthor(),
+                currencyFormat.format(book.getPrice()),
+                book.getStock()
+            };
+            tableModel.addRow(row);
+        });
+        table.setModel(tableModel);
     }
 }
